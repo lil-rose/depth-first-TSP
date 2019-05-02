f4eec7776fb3e4c436d4a6b2b8c78a7145d8cb47
Rosita <rosita.hormannlobos@gmail.com>
2019-05-02 15:59:36 -0400

    Statistics changed to their own module

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
 # from depthfirst import findDepthFirst
 from DepthFirsSearch import DepthFirstSearch
 import GraphUtils
+import StatisticsUtils
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
-def generateStatistics(n):
-    arrayTimes = []
-    for i in range(2,n+1):
-        print('i:', i)
-        randoms = GraphUtils.arrayRandoms(n)
-        graph = GraphUtils.createGraph(randoms, i)
-        DF = DepthFirstSearch(graph, i)
-
-        start = time.time()
-        DF.DepthFirsIterative(0)
-        end = time.time()
-
-        arrayTimes.append(end-start)
-    return arrayTimes
-
-def plotArrayTimes(arrayTimes, n):
-    fig, ax = plt.subplots(figsize=(10,10))
-    # agregando ejes a la figura:
-    arrayN = np.arange(2,n+1)
-    # ax = fig.add_axes([0.0, 0.0, 1.0, 1.0]) #izquierda, abajo, ancho, altura (rango de 0 a 1)
-    ax.plot(arrayN, arrayTimes, 'b')
-    ax.set_xlabel('n') # Notice the use of set_ to begin methods
-    ax.set_ylabel('tiempo (segundos)')
-    ax.set_title('Tiempo de ejecución')
-    # plt.ylim(0,arrayTimes[n-3])
-    plt.xlim(0,n + 1)
-    nameFile = 'estadistica' + str(n) + '.jpg'
-    fig.savefig(nameFile)
-
-def testStatistics():
-    n = 11
-    arrayTimes = generateStatistics(n)
-    plotArrayTimes(arrayTimes, n)
 
 def showPath(depthFirstObj):
     pathStr = '[0]'
@@ -48,31 +15,10 @@ def showPath(depthFirstObj):
         pathStr = pathStr + ' -> ' + str(depthFirstObj.graph[row, col]) + ' -> [' + str(bestSolution[i+1]) + ']'
     return pathStr
 
-
-n = 5
-ejemplo = GraphUtils.arrayRandoms(n)
-
-ejemploGrafo = GraphUtils.createGraph(ejemplo, n)
-
-objetoEjemplo = DepthFirstSearch(ejemploGrafo, n)
-objetoEjemplo.DepthFirsIterative(0)
-
-showPath(objetoEjemplo)
-
-# start = time.time()
-
-
-# objetoEjemplo.findDepthFirst(0)
-
-# end = time.time()
-
-# print('TIEMPO: ', end - start)
-
-### AQUI EMPIEZA EL SCRIPT
 def separation():
     print('\n-----------------------\n')
 
-
+# Script begins
 while n != -1:
     separation()
     print('Ingrese un valor de n (cantidad de ciudades)')
